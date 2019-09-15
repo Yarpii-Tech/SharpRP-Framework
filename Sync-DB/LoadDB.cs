10b09fc4b1baeb79f92754a56b02957ad5e4f1be
sync0s <bryanfassett@gmail.com>
2019-09-15 17:00:38 -0500

    Sync DB (Not Complete)

---
+++ Sync-DB/LoadDB.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Sync_DB
+{
+    public class LoadDB : BaseScript
+    {
+        public MySqlConnection conn = null;
+        private string cs = @"server=localhost;userid=root;password=Overfl0w1@"; // TODO: put this into a local resource file
+
+        public LoadDB()
+        {
+            EventHandlers["onResourceStart"] += new Action<string>(loadDatabase);
+            EventHandlers["onResourceStop"] += new Action<string>(unloadDatabase);
+        }
+
+        private void loadDatabase(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName) return;
+            Debug.WriteLine("Time to load db");
+
+            try
+            {
+                conn = new MySqlConnection(cs);
+                conn.Open();
+                Debug.WriteLine("==========================");
+                Debug.WriteLine("Sync DB Connection Successful");
+                Debug.WriteLine("Mysql version: {0}", conn.ServerVersion);
+                Debug.WriteLine("==========================");
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "CREATE DATABASE IF NOT EXISTS SharpDB; Use SharpDB;";
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS Players(playerid INT(10) NULL, position INT(10) NULL) COLLATE = 'latin1_swedish_ci' ENGINE = InnoDB;";
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Debug.WriteLine($"MySql Errror: {ex}");
+            }
+        }
+
+        private void unloadDatabase(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName) return;
+
+            Debug.WriteLine("Sync DB Connection Closed");
+            conn.Close();
+        }
+    }
+}
--- Sync-DB/Properties/AssemblyInfo.cs
+++ Sync-DB/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MySQL")]
+[assembly: AssemblyTitle("Sync-DB")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MySQL")]
+[assembly: AssemblyProduct("Sync-DB")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8b10a891-4e73-4cdb-a830-1a320b91f525")]
+[assembly: Guid("23009608-ece4-4e15-b9fe-398ccc5bb9a9")]
 
 // Version information for an assembly consists of the following four values:
 //
